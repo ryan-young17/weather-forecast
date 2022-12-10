99df2c5444b278b9f504b1694d27fa2cb57465cc
Ryan Young <ryanbyoung1@gmail.com>
2022-12-10 11:24:05 -0500

    removed extra variables and preventDefault function

--- assets/script.js
+++ assets/script.js
@@ -2,17 +2,16 @@
 var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=2b66bb1041cf5c2ab89f9477dd5f8008"
 
 // Lat/Long API
-var cityUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "," + stateCode + "," + countryCode + "&limit=" + limit + "&appid=2b66bb1041cf5c2ab89f9477dd5f8008"
 
 var lat;
 var lon;
-var cityName = document.querySelector("#searchInput").value;
-var stateCode;
-var countryCode;
-var limit;
-var searchBtn = document.querySelector(".btn-primary");
+var cityName = "New York";
+var limit = 1;
+var searchBtn = document.querySelector(".btn");
+var cityUrl = "http://api.openweathermap.org/geo/1.0/direct?q=austin&limit=1&appid=2b66bb1041cf5c2ab89f9477dd5f8008";
 
-searchBtn.addEventListener("click", function () {
+searchBtn.addEventListener("click", function (event) {
+event.preventDefault();
     fetch(cityUrl)
         .then(function (response) {
             return response.json();
@@ -23,4 +22,6 @@ searchBtn.addEventListener("click", function () {
         .catch(function (error) {
             console.log(error);
         });
-})
+});
+
+// document.querySelector("#searchInput").value;
