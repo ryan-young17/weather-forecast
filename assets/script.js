011dee1c97d690d81065eda87c5bed4643aa95fb
Ryan Young <ryanbyoung1@gmail.com>
2022-12-11 11:48:49 -0500

    added weatherForecast function with forecast api

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,31 @@
-// Weather API
-// var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=2b66bb1041cf5c2ab89f9477dd5f8008"
-
 var searchBtn = document.querySelector(".btn");
 
 var getCityCoordinates = function () {
     var cityName = document.querySelector("#searchInput");
     var cityUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName.value + "&limit=1&appid=2b66bb1041cf5c2ab89f9477dd5f8008";
     fetch(cityUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        var lat = data[0].lat;
+        var lon = data[0].lon;
+        getWeatherForecast(lat, lon);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+};
+
+var getWeatherForecast = function (lat, lon) {
+    var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=2b66bb1041cf5c2ab89f9477dd5f8008";
+    fetch(forecastUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data);
-            // var lat = data.lat;
-            // var lon = data.lon;
         })
         .catch(function (error) {
             console.log(error);
@@ -24,6 +36,5 @@ var getCityCoordinates = function () {
 searchBtn.addEventListener("click", function (event) {
     event.preventDefault();
     getCityCoordinates();
+    getWeatherForecast(lat, lon);
 });
-
-// document.querySelector("#searchInput").value;
