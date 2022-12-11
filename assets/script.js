604a9244b89a07a5cb94c6b46ab4602f4c12d27e
Ryan Young <ryanbyoung1@gmail.com>
2022-12-11 11:25:15 -0500

    moved cityName var in function with api and created function outside of click event

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,29 @@
 // Weather API
-var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=2b66bb1041cf5c2ab89f9477dd5f8008"
+// var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=2b66bb1041cf5c2ab89f9477dd5f8008"
 
-// Lat/Long API
-
-var lat;
-var lon;
-var cityName = "New York";
-var limit = 1;
 var searchBtn = document.querySelector(".btn");
-var cityUrl = "http://api.openweathermap.org/geo/1.0/direct?q=austin&limit=1&appid=2b66bb1041cf5c2ab89f9477dd5f8008";
 
-searchBtn.addEventListener("click", function (event) {
-event.preventDefault();
+var getCityCoordinates = function () {
+    var cityName = document.querySelector("#searchInput");
+    var cityUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName.value + "&limit=1&appid=2b66bb1041cf5c2ab89f9477dd5f8008";
     fetch(cityUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data);
+            // var lat = data.lat;
+            // var lon = data.lon;
         })
         .catch(function (error) {
             console.log(error);
         });
+};
+
+
+searchBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    getCityCoordinates();
 });
 
 // document.querySelector("#searchInput").value;
